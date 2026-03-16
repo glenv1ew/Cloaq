2d80fee0129370c0281b391e2da1dc37261f562e
CartridgeSmoker <montoguyps4@gmail.com>
2026-03-16 14:31:39 +0500

    style: add missing NOTICE headers

--- src/tun/tun_linux.go
+++ src/tun/tun_linux.go
@@ -1,5 +1,19 @@
 //go:build linux
 
+// NOTICE
+
+// Project Name: Cloaq
+// Copyright © 2026 Neil Talap and/or its designated Affiliates.
+
+// This software is licensed under the Dragonfly Public License (DPL) 1.0.
+
+// All rights reserved. The names "Neil Talap" and any associated logos or branding
+// are trademarks of the Licensor and may not be used without express written permission,
+// except as provided in Section 7 of the License.
+
+// For commercial licensing inquiries or permissions beyond the scope of this
+// license, please create an issue in github.
+
 package tun
 
 import (
@@ -41,6 +55,7 @@ func InitDevice() (Device, error) {
 	return &linuxDevice{name: name, f: f}, nil
 
 }
+
 // Read reads packets from the TUN device
 func (d *linuxDevice) Read(buf []byte) (int, error) {
 	if d.f == nil {
@@ -55,4 +70,3 @@ func (d *linuxDevice) Read(buf []byte) (int, error) {
 
 	return n, nil
 }
-
